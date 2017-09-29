67412c2b675c6b070a75e5ce8fa2d5a133a31ff6
pingping520 <32315797+pingping520@users.noreply.github.com>
2017-09-29 18:03:53 +0800

    1
    1

---
+++ src/cn/com/moonisland/mapper/GlassMapper.java
@@ -0,0 +1,24 @@
+package cn.com.moonisland.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import cn.com.moonisland.pojo.Glass;
+
+
+public interface GlassMapper {
+	//全表查询
+		public List<Glass> findall();
+		//id查询
+		public List<Glass> findbyid(int id);
+		//添加
+		public int insert(Glass g);
+		//更新
+		public int update(Glass g);
+		//删除
+		public void delete(Glass g);
+		//分页查询
+		public List<Glass> findlimit(Map<String, Integer> map);
+		//总页数
+		public int pagecount();
+}
