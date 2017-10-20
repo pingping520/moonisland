66358663d6c0f65dbcf87958e087e124ec0e03b8
pingping520 <32315797+pingping520@users.noreply.github.com>
2017-10-20 11:15:48 +0800

    123
    23

--- WebContent/js/sortList.js
+++ WebContent/js/sortList.js
@@ -8,20 +8,19 @@ $(function(){
 })
 //ajax获取数据
 $(function(){
-   var list=$('#goodsList');
-   
+   var list=$('#goodsList'); 
+ //获取商品的图片信息
+
   $.get("../goods/findAllGoods", function(data){
-	 var divlist="";
-	 
+	 var divlist=""; 
      for(var i=0;i<data.length;i++){
     	
     	console.log(data[i]);
-    
-    	divlist+='<div class="row"><div class="col-xs-4"><a href="../html/detailPage.html?goodsId="'+data[i].goodsId+'><img src="images/shop01.png"/></a></div>';
+        divlist+='<div class="row"><div class="col-xs-4"><img src="../images/'+data[i].goodsContent+'"/><a href="../html/detailPage.html?goodsId="'+data[i].goodsId+'></a></div>';
     	divlist+='<div class="col-xs-8"><div class="lb2"><a href="../html/detailPage.html?goodsId='+data[i].goodsId+'">'+data[i].goodsName+'</a></div>';
     	divlist+='<div class="lb3"><strong class="J_01" data-bone="1"><em>￥'+data[i].salesPrice+'</em><i>.00</i></strong>';
     	divlist+='<div class="pull-right">新品</div></div>';
-    	divlist+=' <div class="lb4"><span><a id="hp1" href="#">8349条评价</a><a id="hp2" href="#">96%好评</a></span></div></div></div>';	
+    	divlist+='<div class="lb4"><span><a id="hp1" href="#">8349条评价</a><a id="hp2" href="#">96%好评</a></span></div></div></div>';	
     	
      }
       list.append(divlist);
